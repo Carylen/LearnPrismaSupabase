a4c4e1af47d95cf4f681e07711f8123f2f251f2c
Carylen <irham.atmoko@ids.id>
2024-06-09 12:14:33 +0700

    second commit, Supabase and Vercel Config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const userController = require("./users/userController")
-
+const userController = require("./users/userController");
+const { createClient } = require("@supabase/supabase-js");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_KEY = process.env.SUPABASE_KEY
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 // const prisma = connection;
 
 app.use(express.json());
 
-app.use('/users', userController)
+app.use("/users", userController);
 
 app.listen(PORT, () => {
   console.log(`Express are listening on PORT:${PORT}`);
