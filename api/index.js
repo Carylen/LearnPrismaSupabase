905d08fec0b13353aa19c70dc2be325d127a3b49
Carylen <irham.atmoko@ids.id>
2024-06-09 15:46:04 +0700

    eighth commit, Supabase and Vercel Config

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const userController = require("./users/userController");
-// const { createClient } = require("@supabase/supabase-js");
+const userController = require("../src/users/userController");
+const { createClient } = require("@supabase/supabase-js");
 const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-// const SUPABASE_URL = process.env.SUPABASE_URL
-// const SUPABASE_KEY = process.env.SUPABASE_KEY
+const SUPABASE_URL = process.env.SUPABASE_URL
+const SUPABASE_KEY = process.env.SUPABASE_KEY
 
-// const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 // const prisma = connection;
 app.use(cors());
 app.use(express.json());
